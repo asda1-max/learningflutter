9fc60bcebfe00b3e9192cf2208fb6a27a8bcd63c
asda1-max <rakha13141@gmail.com>
2025-02-15 10:58:02 +0700

    Deskripsi perubahan

--- lib/main.dart
+++ lib/main.dart
@@ -10,25 +10,27 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: const Color.fromARGB(255, 230, 83, 15),
-          title: const Text('flata'),
-          
+        home: Scaffold(
+      appBar: AppBar(
+        backgroundColor: const Color.fromARGB(255, 230, 83, 15),
+        title: const Text('flata'),
+      ),
+      body: Center(
+        child: Container(
+          child: const Text('Test Widget 1'),
+          margin: const EdgeInsets.all(100),
+          padding: const EdgeInsets.all(10),
+          height: 100,
+          width: 100,
+          color: Colors.blueGrey,
         ),
-        body: Center(
-          child: Container(
-            child: const Text('wassup nigger'),
-            margin: const EdgeInsets.all(100),
-            padding: const EdgeInsets.all(10),
-            height: 100,
-            width: 100,
-            color: Colors.blueGrey,
-            
-          ),
-        )
-      )
-    );
-  
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          print('woi');
+        },
+      ),
+    ));
   }
 }
